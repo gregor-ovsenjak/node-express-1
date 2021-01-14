a84ece0c6447f119960ea3f4ad21a17d38d57f72
GregorOvsenjak <gregor.ovsenjak@gmail.com>
2021-01-14 18:34:03 +0100

    Added production object to knexfile

--- knexfile.js
+++ knexfile.js
@@ -8,10 +8,22 @@ module.exports = {
       filename: './data/lessons.db3'
     },
     useNullAsDefault:true,
+    pool: {
+      afterCreate:(conn,done)=> {
+        conn.run('PRAGMA foreign_keys = ON',done);
+      },
+    },
   },
-  pool: {
-    afterCreate:(conn,done)=> {
-      conn.run('PRAGMA foreign_keys = ON',done);
+  production : {
+    client:'pg',
+    connection : process.env.DATABASE_URL,
+    pool:{
+      min:2,
+      max:10
+    },
+    migrations: {
+      tablename: 'knex_migrations',
+      directory: './migrations',
     }
-  }
+  },
 };
